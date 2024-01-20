f57f90b764efbf3f2f1b894f4ae85f3b231723b0
Ronaldo Correia <ronaldo312006@hotmail.com>
2024-01-20 17:11:17 -0300

    Sistema de reset de senha
    Fiz a parte de reset de senha criando as urls e os templates correspondente para cada view.

--- free_lance_pro/settings.py
+++ free_lance_pro/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 from django.contrib.messages import constants
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,3 +145,14 @@ MESSAGE_TAGS = {
 
 
 AUTH_USER_MODEL = 'free_lance_app.User'
+
+# Envio de E-mail
+
+DEFAULT_FROM_EMAIL = "ronaldocorreiadesouza@gmail.com"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+
